543119b0a80728ef29f211dbb624bd42e86e05b0
dodos <dodos@Dodos-Mac.local>
2026-05-30 07:20:02 -0400

    feat: Add JSON5 and YAML file extraction support

--- src/json_extract_pandas/__init__.py
+++ src/json_extract_pandas/__init__.py
@@ -1,4 +1,4 @@
-from .json_extract import extract_json, _flatten_and_expand
+from .json_extract import extract_json, _flatten_and_expand, extract_file
 
-__version__ = "0.1.9"
-__all__ = ["extract_json", "_flatten_and_expand"]
+__version__ = "0.1.12"
+__all__ = ["extract_json", "_flatten_and_expand", "extract_file"]
